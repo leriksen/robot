4606931fefa198a68c6098ee38b114bf573f352a
leif.eriksen <leif.eriksen@iag.com.au>
2015-10-27 21:31:01 +1100

    again classic TDD - declare how we want to create a table, we'll write the code later.
    this basic test will fail until we get the constructor right. We
    explain some reasoning in comments too.

--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Table do
-  
+  # it should take a height and a width, as not all tables are square
+  subject {described_class.new(5,5)}
+
+  context '#new' do
+    it { is_expected.to be_a(described_class) }
+  end  
 end
