d23f2c4656dec93d902ed3e05ff9e083943313b1
leif.eriksen <leif.eriksen@iag.com.au>
2015-10-27 22:10:24 +1100

    the table knows where its edges are, and if you're facing over its edge.
    we could provide accessors on table edges, then robots can contain this
    logic, but I think this feels better. We may rename to
    is_dangerous_position? if the code doesn't read to well when we get to
    modelling the robot and its moves.

--- lib/table.rb
+++ lib/table.rb
@@ -5,6 +5,20 @@ class Table
     @height = height
   end
 
+  # let model that the table knows where its edges are and what way you're facing when looking over the edge
+  
   def facing_edge?(x, y, facing)
+    case facing
+    when :east
+      x == @width
+    when :west
+      x == 0
+    when :north
+      y == @height
+    when :south
+      y == 0
+    else
+      fail "Unknown cardinal compass point #{facing}"
+    end 
   end
 end
--- spec/table_spec.rb
+++ spec/table_spec.rb
@@ -10,5 +10,29 @@ describe Table do
 
   context '.facing_edge?(x,y,facing)' do
     it { is_expected.to respond_to(:facing_edge?) }
+
+    it "detects the southern edge" do
+      expect(subject.facing_edge? 0, 0, :south).to be_truthy
+      expect(subject.facing_edge? 1, 1, :south).to be_falsey
+    end
+
+    it "detects the northern edge" do
+      expect(subject.facing_edge? 5, 5, :north).to be_truthy
+      expect(subject.facing_edge? 1, 1, :north).to be_falsey
+    end
+
+    it "detects the western edge" do
+      expect(subject.facing_edge? 0, 0, :west).to be_truthy
+      expect(subject.facing_edge? 4, 4, :west).to be_falsey
+    end
+
+    it "detects the eastern edge" do
+      expect(subject.facing_edge? 5, 5, :east).to be_truthy
+      expect(subject.facing_edge? 4, 4, :east).to be_falsey
+    end
+
+    it "only accepts cardinal points" do
+      expect{subject.facing_edge? 2, 2, :up}.to raise_exception(RuntimeError, /Unknown cardinal compass point/)
+    end
   end
 end
