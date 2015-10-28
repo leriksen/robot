a3fabe72eff3fcb49846feb599c892df50902ff0
leif.eriksen <leif.eriksen@iag.com.au>
2015-10-28 14:26:18 +1100

    parser is enumerable - sorta
    so i've kinda shot myself in the foot by trying to show off - but it
    works for the cases i need it to. i thought Enumerable mixed in more
    methods than it does, so the interface isnt as clean as i hoped and
    there will be a bit of jiggery-pokery next as we build the command
    runner. the enumerator also has the nasty destructive list processing
    (the call to shift), but i'm not willing right now to spend the time
    removing this unwanted side-effect. i also wont spend the time to
    incorporate lazy enumeration and have the robot process potentially
    infinite-length command lists. i think mentioning it is enough to show
    my thought-processes.

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,2 +1,28 @@
 class Parser
+
+  include Enumerable
+
+  def initialize(file)
+    @lines = file.readlines.map(&:chomp).map(&:strip).map(&:downcase)
+  end
+
+  def next
+    process_line
+  end
+
+  def each
+    loop do
+      yield self.next
+    end
+  end
+
+  private
+  def process_line
+    line = @lines.shift
+
+    raise StopIteration unless line
+
+    elements = line.split(',')
+    [elements.shift.to_sym, elements]
+  end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Parser do
-  
+  let(:file) {
+    StringIO.new <<-EOS
+      PLACE,0,0,north
+      MOVE
+      REPORT
+    EOS
+  }
+
+  subject {described_class.new file}
+  context 'reading input' , focus: true do
+    it 'can iterate' do
+      expect(subject).to respond_to(:each)
+
+      list = []
+      subject.each do |command, args|
+        list << command
+      end
+      expect(list.length).to eql(3)
+    end
+
+    it 'can map' do
+      result = subject.map {|command, args|
+        if command == :place
+          [command, [args[0].to_i, args[1].to_i, args[2].to_sym]]
+        else
+          [command]
+        end
+      }
+      expect(result).to eql([[:place, [0, 0, :north]], [:move],  [:report]])
+    end
+  end
 end
