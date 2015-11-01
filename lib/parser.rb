9706790cc1dd9e45f46b21d22e67ea9ea292aebe
leif.eriksen <leif.eriksen@iag.com.au>
2015-11-01 19:45:32 +1100

    the enumerate was all wrong - or rather it worked for the wrong reasons
    rework to use a proper Enumerator, that we can operate on sensibly with
    the next() operations.

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,27 +2,24 @@ class Parser
 
   include Enumerable
 
-  def initialize(file)
-    @lines = file.readlines.map(&:chomp).map(&:strip).map(&:downcase)
-  end
-
-  def next
-    process_line
+  def initialize(io)
+    # returns raw enumerator
+    @io = io.each
   end
 
   def each
     loop do
-      yield self.next
+      yield process_line(io.next)
     end
   end
 
   private
-  def process_line
-    line = @lines.shift
-
-    raise StopIteration unless line
-
-    (command, args) = line.split(' ')
-    [command.to_sym, args.nil? ? args : args.split(',')]
+  def process_line(line)
+    if line
+      (command, args) = line.downcase.strip.split(' ')
+      [command.to_sym, args.nil? ? args : args.split(',')]
+    end
   end
+
+   attr_reader :io
 end
